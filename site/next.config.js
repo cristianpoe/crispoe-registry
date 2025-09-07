237256d27f5a6b7733a76894a685f4f3b4e6bd8f
cristianpoe <83978893+cristianpoe@users.noreply.github.com>
2025-09-07 18:57:19 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'CrisPoe Registry',
     description: 'The official store for CrisPoe Custom workspaces.',
-    icon: 'https://github.com/cristianpoe/crispoe-registry/blob/c7addece34e24aefbf8935a3fbcdf573a64d509c/CrisPoe_Registry.png',
+    icon: 'https://raw.githubusercontent.com/cristianpoe/crispoe-registry/refs/heads/1.1/CrisPoe_Registry.png',
     listUrl: 'https://cristianpoe.github.io/crispoe-registry/',
     contactUrl: 'https://github.com/cristianpoe/crispoe-registry/issues',
   },
